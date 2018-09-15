2aba42bab3fa334551419193d97f8353554ca4eb
Carolina Thomaz <carol.com.jp@gmail.com>
2018-09-15 13:46:26 -0300

    Projeto criado

---
+++ src/main/java/com/carolchang/contatoservice/ContatoServiceApplication.java
@@ -0,0 +1,12 @@
+package com.carolchang.contatoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContatoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContatoServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/carolchang/contatoservice/ContatoServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.carolchang.contatoservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ContatoServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
